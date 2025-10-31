a568f951b152e8095fca2304c812d204e3391381
Ayush Dixit <fsayush100@gmail.com>
2025-10-31 16:07:23 +0530

    Integrate TopLoader component into the layout for improved loading feedback. Refactor Providers component to remove NextTopLoader and streamline the structure, enhancing code maintainability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
+import TopLoader from "@/components/TopLoader";
 
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopLoader />
         <Providers>
           {children}
         </Providers>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,21 +2,13 @@
 
 import { UserProvider } from "@/contexts/UserContext"
 import { ToastContainer } from "react-toastify"
-import NextTopLoader from 'nextjs-toploader'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <NextTopLoader
-        showSpinner={true}
-        color="#0f766e"
-        height={3}
-      />
-      <UserProvider>
-        <ToastContainer />
-        {children}
-      </UserProvider>
-    </>
+    <UserProvider>
+      <ToastContainer />
+      {children}
+    </UserProvider>
   )
 }
 
