bdcd57ca83fb0d7e3fd2f4cbea648e1f1862b80c
Ayush Dixit <fsayush100@gmail.com>
2025-11-28 12:27:51 +0530

    Add Google Analytics integration by including gtag.js script and initialization code in the RootLayout component. This enhances tracking capabilities for user interactions on the site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
 import TopLoader from "@/components/TopLoader";
@@ -30,6 +31,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-851334617"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-851334617');
+          `}
+        </Script>
         <TopLoader />
         <Providers>
           {children}
